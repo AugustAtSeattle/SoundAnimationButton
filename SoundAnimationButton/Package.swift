e50038d84a4e901b80c2871a5cc707b3fa82bebd
AugustAtSeattle <august.lin.usa@gmail.com>
2024-01-03 11:48:41 -0800

    import SCAudioManager into SoundAnimationButton Library

--- SoundAnimationButton/Package.swift
+++ SoundAnimationButton/Package.swift
@@ -20,7 +20,12 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SoundAnimationButton",
-            dependencies: []),
+            dependencies: ["SCAudioManager"]),
+        .systemLibrary(
+            name: "SCAudioManager",
+            path: "Sources/SCAudioManager",
+            pkgConfig: "sc_audio_manager"
+        ),
         .testTarget(
             name: "SoundAnimationButtonTests",
             dependencies: ["SoundAnimationButton"]),
